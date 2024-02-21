1197e6d53bb3a9b1e4d8dfe09a1e446d939c37b6
Ritu Gatiyala <ritu.gatiyala@toptal.com>
2024-02-21 15:54:30 +0530

    Adds modal to the body instead of cntent using portals

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, timeRemaining, onReset },
@@ -17,7 +18,7 @@ const ResultModal = forwardRef(function ResultModal(
       },
     };
   });
-  return (
+  return createPortal(
     //By defsult the dialog is invisible but we can make it visible by seeting open attribute
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You Lost!</h2>}
@@ -32,7 +33,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
